4da0f429acc8af1ee456a21d02277eaaed1379c8
Matej Snuderl <ematej.snuderl@gmail.com>
2020-08-31 10:43:37 +0200

    support title

--- src/utils.js
+++ src/utils.js
@@ -1,16 +1,16 @@
-import fs from 'fs';
-import prettyBytes from 'pretty-bytes';
+import fs from "fs";
+import prettyBytes from "pretty-bytes";
 
 /**
  * Check if a given file exists and can be accessed.
  * @param {string} filename
  */
 export async function fileExists(filename) {
-	try {
-		await fs.promises.access(filename, fs.constants.F_OK);
-		return true;
-	} catch (e) {}
-	return false;
+  try {
+    await fs.promises.access(filename, fs.constants.F_OK);
+    return true;
+  } catch (e) {}
+  return false;
 }
 
 /**
@@ -19,24 +19,24 @@ export async function fileExists(filename) {
  * @returns {(((fileName: string) => string) | undefined)}
  */
 export function stripHash(regex) {
-	if (regex) {
-		console.log(`Striping hash from build chunks using '${regex}' pattern.`);
-		return function (fileName) {
-			return fileName.replace(new RegExp(regex), (str, ...hashes) => {
-				hashes = hashes.slice(0, -2).filter((c) => c != null);
-				if (hashes.length) {
-					for (let i = 0; i < hashes.length; i++) {
-						const hash = hashes[i] || '';
-						str = str.replace(hash, hash.replace(/./g, '*'));
-					}
-					return str;
-				}
-				return '';
-			});
-		};
-	}
+  if (regex) {
+    console.log(`Striping hash from build chunks using '${regex}' pattern.`);
+    return function (fileName) {
+      return fileName.replace(new RegExp(regex), (str, ...hashes) => {
+        hashes = hashes.slice(0, -2).filter((c) => c != null);
+        if (hashes.length) {
+          for (let i = 0; i < hashes.length; i++) {
+            const hash = hashes[i] || "";
+            str = str.replace(hash, hash.replace(/./g, "*"));
+          }
+          return str;
+        }
+        return "";
+      });
+    };
+  }
 
-	return undefined;
+  return undefined;
 }
 
 /**
@@ -44,27 +44,27 @@ export function stripHash(regex) {
  * @param {number} difference
  */
 export function getDeltaText(delta, difference) {
-	let deltaText = (delta > 0 ? '+' : '') + prettyBytes(delta);
-	if (delta && Math.abs(delta) > 1) {
-		deltaText += ` (${Math.abs(difference)}%)`;
-	}
-	return deltaText;
+  let deltaText = (delta > 0 ? "+" : "") + prettyBytes(delta);
+  if (delta && Math.abs(delta) > 1) {
+    deltaText += ` (${Math.abs(difference)}%)`;
+  }
+  return deltaText;
 }
 
 /**
  * @param {number} difference
  */
 export function iconForDifference(difference) {
-	let icon = '';
-	if (difference >= 50) icon = '🆘';
-	else if (difference >= 20) icon = '🚨';
-	else if (difference >= 10) icon = '⚠️';
-	else if (difference >= 5) icon = '🔍';
-	else if (difference <= -50) icon = '🏆';
-	else if (difference <= -20) icon = '🎉';
-	else if (difference <= -10) icon = '👏';
-	else if (difference <= -5) icon = '✅';
-	return icon;
+  let icon = "";
+  if (difference >= 50) icon = "🆘";
+  else if (difference >= 20) icon = "🚨";
+  else if (difference >= 10) icon = "⚠️";
+  else if (difference >= 5) icon = "🔍";
+  else if (difference <= -50) icon = "🏆";
+  else if (difference <= -20) icon = "🎉";
+  else if (difference <= -10) icon = "👏";
+  else if (difference <= -5) icon = "✅";
+  return icon;
 }
 
 /**
@@ -72,31 +72,33 @@ export function iconForDifference(difference) {
  * @param {string[]} rows
  */
 function markdownTable(rows) {
-	if (rows.length == 0) {
-		return '';
-	}
+  if (rows.length == 0) {
+    return "";
+  }
 
-	// Skip all empty columns
-	while (rows.every(columns => !columns[columns.length - 1])) {
-		for (const columns of rows) {
-			columns.pop();
-		}
-	}
+  // Skip all empty columns
+  while (rows.every((columns) => !columns[columns.length - 1])) {
+    for (const columns of rows) {
+      columns.pop();
+    }
+  }
 
-	const [firstRow] = rows;
-	const columnLength = firstRow.length;
-	if (columnLength === 0) {
-		return '';
-	}
+  const [firstRow] = rows;
+  const columnLength = firstRow.length;
+  if (columnLength === 0) {
+    return "";
+  }
 
-	return [
-		// Header
-		['Filename', 'Size', 'Change', ''].slice(0, columnLength),
-		// Align
-		[':---', ':---:', ':---:', ':---:'].slice(0, columnLength),
-		// Body
-		...rows
-	].map(columns => `| ${columns.join(' | ')} |`).join('\n');
+  return [
+    // Header
+    ["Filename", "Size", "Change", ""].slice(0, columnLength),
+    // Align
+    [":---", ":---:", ":---:", ":---:"].slice(0, columnLength),
+    // Body
+    ...rows,
+  ]
+    .map((columns) => `| ${columns.join(" | ")} |`)
+    .join("\n");
 }
 
 /**
@@ -114,52 +116,56 @@ function markdownTable(rows) {
  * @param {boolean} [options.collapseUnchanged]
  * @param {boolean} [options.omitUnchanged]
  * @param {number} [options.minimumChangeThreshold]
+ * @param {string} [options.title]
  */
-export function diffTable(files, { showTotal, collapseUnchanged, omitUnchanged, minimumChangeThreshold }) {
-	let changedRows = [];
-	let unChangedRows = [];
+export function diffTable(
+  files,
+  { showTotal, collapseUnchanged, omitUnchanged, minimumChangeThreshold, title }
+) {
+  let changedRows = [];
+  let unChangedRows = [];
 
-	let totalSize = 0;
-	let totalDelta = 0;
-	for (const file of files) {
-		const { filename, size, delta } = file;
-		totalSize += size;
-		totalDelta += delta;
+  let totalSize = 0;
+  let totalDelta = 0;
+  for (const file of files) {
+    const { filename, size, delta } = file;
+    totalSize += size;
+    totalDelta += delta;
 
-		const difference = ((delta / size) * 100) | 0;
-		const isUnchanged = Math.abs(delta) < minimumChangeThreshold;
+    const difference = ((delta / size) * 100) | 0;
+    const isUnchanged = Math.abs(delta) < minimumChangeThreshold;
 
-		if (isUnchanged && omitUnchanged) continue;
+    if (isUnchanged && omitUnchanged) continue;
 
-		const columns = [
-			`\`${filename}\``, 
-			prettyBytes(size), 
-			getDeltaText(delta, difference), 
-			iconForDifference(difference)
-		];
-		if (isUnchanged && collapseUnchanged) {
-			unChangedRows.push(columns);
-		} else {
-			changedRows.push(columns);
-		}
-	}
+    const columns = [
+      `\`${filename}\``,
+      prettyBytes(size),
+      getDeltaText(delta, difference),
+      iconForDifference(difference),
+    ];
+    if (isUnchanged && collapseUnchanged) {
+      unChangedRows.push(columns);
+    } else {
+      changedRows.push(columns);
+    }
+  }
 
-	let out = markdownTable(changedRows);
+  let out = markdownTable(changedRows);
 
-	if (unChangedRows.length !== 0) {
-		const outUnchanged = markdownTable(unChangedRows);
-		out += `\n\n<details><summary>ℹ️ <strong>View Unchanged</strong></summary>\n\n${outUnchanged}\n\n</details>\n\n`;
-	}
+  if (unChangedRows.length !== 0) {
+    const outUnchanged = markdownTable(unChangedRows);
+    out += `\n\n<details><summary>ℹ️ <strong>View Unchanged</strong></summary>\n\n${outUnchanged}\n\n</details>\n\n`;
+  }
 
-	if (showTotal) {
-		const totalDifference = ((totalDelta / totalSize) * 100) | 0;
-		let totalDeltaText = getDeltaText(totalDelta, totalDifference);
-		let totalIcon = iconForDifference(totalDifference);
-		out = `**Total Size:** ${prettyBytes(totalSize)}\n\n${out}`;
-		out = `**Size Change:** ${totalDeltaText} ${totalIcon}\n\n${out}`;
-	}
+  if (showTotal) {
+    const totalDifference = ((totalDelta / totalSize) * 100) | 0;
+    let totalDeltaText = getDeltaText(totalDelta, totalDifference);
+    let totalIcon = iconForDifference(totalDifference);
+    out = `**Total Size:** ${prettyBytes(totalSize)}\n\n${out}`;
+    out = `**Size Change:** ${totalDeltaText} ${totalIcon}\n\n${out}`;
+  }
 
-	return out;
+  return title ? `# ${title} \n\n ${out}` : out;
 }
 
 /**
@@ -167,5 +173,5 @@ export function diffTable(files, { showTotal, collapseUnchanged, omitUnchanged, 
  * @param {string} v
  */
 export function toBool(v) {
-	return /^(1|true|yes)$/.test(v);
+  return /^(1|true|yes)$/.test(v);
 }
